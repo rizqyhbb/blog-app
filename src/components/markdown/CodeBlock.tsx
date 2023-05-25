ef2d75bc74250b7d1d7f2c4ecbbc59e68eb5608f
rizqyhbb <rizqyhbb@gmail.com>
2023-05-25 10:30:52 +0700

    Add copy to clipboard function

--- src/components/markdown/CodeBlock.tsx
+++ src/components/markdown/CodeBlock.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
@@ -12,11 +12,23 @@ interface ICode {
 const CodeBlock: FC<ICode> = ({ children }) => {
   const { className, children: code } = children.props;
   const language = className?.replace("lang-", "");
+  const [isCopied, setIsCopied] = useState<boolean>(false)
+
+  const handleCopy = () => {
+    try {
+      navigator.clipboard.writeText(code)
+      setIsCopied(true)
+      setTimeout(() => setIsCopied(false), 2000)
+    } catch (error) {
+      return
+    }
+  }
+
   return (
     <div>
-      <div className='flex justify-between item-scenter bg-slate-600 px-4 py-1 rounded-t-lg text-white'>
-        <span>{language}</span>
-        <button>Copy</button>
+      <div className='flex justify-between item-scenter bg-slate-600 px-4 py-2 rounded-t-lg text-white'>
+        <span className='text-xs'>{language}</span>
+        <button className='text-xs' onClick={handleCopy}>{isCopied ? "Copied!" : "Copy"}</button>
       </div>
       <SyntaxHighlighter language={language} style={a11yDark} customStyle={{ marginTop: 0, borderRadius: "0 0 8px 8px", }}>
         {code}
