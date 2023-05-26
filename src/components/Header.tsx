757913f52b5e701486ebe82ded2aebd4386aedbd
rizqyhbb <rizqyhbb@gmail.com>
2023-05-26 17:15:14 +0700

    Add volta and icon for portfolio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import LargeLogo from './logo/LargeLogo'
 import MiniLogo from './logo/MiniLogo'
+import Icon from './Icon'
+import LogoPortfolio from './logo/LogoPortfolio'
 
 const Header = () => {
   return (
     <div className='mx-auto px-5 md:px-14 lg:max-w-6xl lg:py-4'>
-      <div className='hidden lg:block'>
+      <div className='hidden lg:flex items-center gap-6'>
         <LargeLogo />
+        <span className='text-2xl font-bold'>X</span>
+        <div className='h-16 w-16 -ml-3'>
+          <LogoPortfolio />
+        </div>
       </div>
-      <div className='block lg:hidden'>
+      <div className='flex items-center gap-4 lg:hidden'>
         <MiniLogo />
+        <span className='text-2xl font-bold'>X</span>
+        <div className='h-12 w-12 -ml-1'>
+          <LogoPortfolio />
+        </div>
       </div>
     </div>
   )
---
+++ src/components/logo/LogoPortfolio.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+import Icon from '../Icon'
+
+const LogoPortfolio = () => {
+  return (
+    <div>
+      <Link href={"https://rizqyhbb.vercel.app"}>
+        <Icon.LogoPortfolio />
+      </Link>
+    </div>
+  )
+}
+
+export default LogoPortfolio
